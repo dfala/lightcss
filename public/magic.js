51b68b7adaee30b8cd122ccd7df62cd13387e609
Daniel Falabella <dnlfala@gmail.com>
2015-09-21 19:31:02 -0600

    Changed h4 and added classes to navigation menu

--- public/magic.js
+++ public/magic.js
@@ -2,18 +2,47 @@
 
 (function () {
 
-  window.onscroll = function (e) {  
-    
-    var scroll = $(window).scrollTop();
+  window.onscroll = function (e) {
 
-    if (($(window).innerHeight() + scroll + 336) >= $("body").height()) {
-      var bottom = scroll - 1210;
+
+    var scrollTop     = $(window).scrollTop(),
+        font          = ($('#font-title').offset().top) - scrollTop,
+        buttons       = ($('#buttons-title').offset().top) - scrollTop,
+        messages      = ($('#messages-title').offset().top) - scrollTop,
+        inputs         = ($('#inputs-title').offset().top) - scrollTop,
+        headers       = ($('#headers-title').offset().top) - scrollTop;
+
+    if (headers <= 0) {
+      makeActive('#headers');
+    } else if (inputs <= 0) {
+      makeActive('#inputs');
+    } else if (messages <= 0) {
+      makeActive('#messages');
+    } else if (buttons <= 0) {
+      makeActive('#buttons');
+    } else if (font <= 0) {
+      makeActive('#font');
+    }
+
+
+    function makeActive (id) {
+      $("#side-menu>div").removeClass("active");
+      $(id).addClass('active');
+    }
+
+
+    if (($(window).innerHeight() + scrollTop + 336) >= $("body").height()) {
+      var bottom = scrollTop - 900;
+      console.log(bottom);
+
+      $("#side-menu>div").removeClass("active");
+      $('#headers').addClass('active');
 
       // $('#side-menu').css('top', 'inherit');
       // $('#side-menu').css('bottom', bottom);
 
     }
-  } 
+  }
 
 
 })();
