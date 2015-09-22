4aa0552e1d893f9b3ac4e22ab6366e69dace8916
Daniel Falabella <dnlfala@gmail.com>
2015-09-21 19:59:59 -0600

    Making the navigation menu work.

--- public/magic.js
+++ public/magic.js
@@ -1,48 +1,71 @@
-// DEPREACTED -- STICKY SIDE MENU
-
 (function () {
+
+  var headersElem   = $('#headers'),
+      inputsElem    = $('#inputs'),
+      messagesElem  = $('#messages'),
+      buttonsElem   = $('#buttons'),
+      fontElem      = $('#font');
+
+
+  // Smooth scrolling
+  headersElem.on('click', function () {
+    scrollTo('#headers-title');
+  });
+
+  inputsElem.on('click', function () {
+    scrollTo('#inputs-title');
+  });
+
+  messagesElem.on('click', function () {
+    scrollTo('#messages-title');
+  });
+
+  buttonsElem.on('click', function () {
+    scrollTo('#buttons-title');
+  });
+
+  fontElem.on('click', function () {
+    scrollTo('#font-title');
+  });
+
+
+  function scrollTo (destination) {
+    $('html, body').animate({
+      scrollTop: ( $(destination).offset().top - 90)
+    }, 750);
+  }
+
+  // Make menu active
+  function makeActive (elem) {
+    $("#side-menu>div").removeClass("active");
+    elem.addClass('active');
+  }
+
 
   window.onscroll = function (e) {
 
-
     var scrollTop     = $(window).scrollTop(),
-        font          = ($('#font-title').offset().top) - scrollTop,
-        buttons       = ($('#buttons-title').offset().top) - scrollTop,
-        messages      = ($('#messages-title').offset().top) - scrollTop,
-        inputs         = ($('#inputs-title').offset().top) - scrollTop,
-        headers       = ($('#headers-title').offset().top) - scrollTop;
+        font          = ($('#font-title').offset().top) - scrollTop - 200,
+        buttons       = ($('#buttons-title').offset().top) - scrollTop - 200,
+        messages      = ($('#messages-title').offset().top) - scrollTop - 200,
+        inputs         = ($('#inputs-title').offset().top) - scrollTop - 200,
+        headers       = ($('#headers-title').offset().top) - scrollTop - 200;
 
-    if (headers <= 0) {
-      makeActive('#headers');
+    if (($(window).innerHeight() + scrollTop + 100) >= $("body").height()) {
+      makeActive(headersElem);
+    } else if (headers <= 0) {
+      makeActive(headersElem);
     } else if (inputs <= 0) {
-      makeActive('#inputs');
+      makeActive(inputsElem);
     } else if (messages <= 0) {
-      makeActive('#messages');
+      makeActive(messagesElem);
     } else if (buttons <= 0) {
-      makeActive('#buttons');
+      makeActive(buttonsElem);
     } else if (font <= 0) {
-      makeActive('#font');
-    }
-
-
-    function makeActive (id) {
-      $("#side-menu>div").removeClass("active");
-      $(id).addClass('active');
-    }
-
-
-    if (($(window).innerHeight() + scrollTop + 336) >= $("body").height()) {
-      var bottom = scrollTop - 900;
-      console.log(bottom);
-
-      $("#side-menu>div").removeClass("active");
-      $('#headers').addClass('active');
-
-      // $('#side-menu').css('top', 'inherit');
-      // $('#side-menu').css('bottom', bottom);
-
+      makeActive(fontElem);
     }
   }
+
 
 
 })();
